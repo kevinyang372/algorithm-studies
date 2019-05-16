b4fd4e22db6e3dbc3997c63e92a91f758e386c90
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-05-16 18:42:44 +0800

    add_digits

---
+++ add_digits.py
@@ -0,0 +1,15 @@
+# Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+# Example:
+
+# Input: 38
+# Output: 2 
+# Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+#              Since 2 has only one digit, return it.
+
+def addDigits(num):
+
+    if len(str(num)) == 1:
+        return int(num)
+
+    return addDigits(sum([int(i) for i in str(num)]))
