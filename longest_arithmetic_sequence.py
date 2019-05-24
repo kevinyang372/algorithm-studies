d67e26b54c0ea8aef39d6c5c87c709cddfe4d64b
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-05-24 18:42:39 +0800

    LAS

--- longest_arithmetic_sequence.py
+++ longest_arithmetic_sequence.py
@@ -28,3 +28,23 @@ def lengthOfLIS(nums):
                 LIS[i] = max(LIS[i], LIS[j] + 1)
 
     return max(LIS)
+
+
+def lengthOfLAS(nums):
+
+    LAS = [1] + [2 for t in range(1, len(nums))]
+    LAS_val = [None] + [[nums[t] - nums[0]] for t in range(1, len(nums))]
+
+    for i in range(1, len(nums)):
+        for j in range(1, i):
+            if nums[i] - nums[j] in LAS_val[j]:
+                if LAS[i] == LAS[j] + 1:
+                    LAS_val[i].append(nums[i] - nums[j])
+                elif LAS[i] < LAS[j] + 1:
+                    LAS[i] = LAS[j] + 1
+                    LAS_val[i] = [nums[i] - nums[j]]
+            elif LAS[i] == 2:
+                LAS_val[i].append(nums[i] - nums[j])
+
+    return max(LAS)
+
