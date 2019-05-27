31a7fd659c4024d56bad59ca48acd1663b9769cb
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-05-27 19:15:49 +0800

    maximum product subarray recursive solution

---
+++ maximum_product_subarray.py
@@ -0,0 +1,24 @@
+# Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+# Example 1:
+
+# Input: [2,3,-2,4]
+# Output: 6
+# Explanation: [2,3] has the largest product 6.
+# Example 2:
+
+# Input: [-2,0,-1]
+# Output: 0
+# Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+def maxProduct(nums):
+
+    if len(nums) == 1:
+        return nums[0]
+
+    times_total = 1
+
+    for i in nums:
+        times_total *= i
+
+    return max(times_total, maxProduct(nums[1:]), maxProduct(nums[:-1]))
