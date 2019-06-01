952329edbf049adf5ea016ab462441cadf9809dc
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-06-01 20:49:44 +0800

    number of islands

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -35,3 +35,38 @@ def longestPalindrome(s: str) -> str:
         p2 = longestPalindrome(s[:-1])
 
         return p1 if len(p1) > len(p2) else p2
+
+def longestPalindrome(s:str) -> str:
+
+    length = len(s)
+    arr_2d = [[False for _ in range(length)] for _ in range(length)]
+    
+    max_sub_len = 0
+    substring = (0, 0)
+    
+    # Trivial case
+    for i in range(length):
+        arr_2d[i][i] = True
+    
+    # Two chars base case
+    for i in range(length-1):
+        j = i + 1
+        arr_2d[i][j] = s[i] == s[j]
+        if arr_2d[i][j] and (2 > max_sub_len):
+            max_sub_len = 2
+            substring = (i, j)
+    
+    # General case
+    step = 2
+    for _ in range(length-2):
+        i = 0
+        j = i+step
+        while (i < length) and (j < length):
+            arr_2d[i][j] = (s[i] == s[j]) and arr_2d[i+1][j-1]
+            if arr_2d[i][j] and (j-i+1 > max_sub_len):
+                max_sub_len = j-i+1
+                substring = (i, j)
+            i += 1
+            j += 1
+        step += 1
+    return s[substring[0]: substring[1]+1]
---
+++ number_of_islands.py
@@ -0,0 +1,65 @@
+# Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+# Example 1:
+
+# Input:
+# 11110
+# 11010
+# 11000
+# 00000
+
+# Output: 1
+# Example 2:
+
+# Input:
+# 11000
+# 11000
+# 00100
+# 00011
+
+# Output: 3
+
+def numIslands(grid):
+
+    if not grid:
+        return 0
+
+    width = len(grid[0])
+    height = len(grid)
+
+    islands = [[0 for _ in range(width)] for _ in range(height)]
+    counter = 1
+
+    for m in range(height):
+        for n in range(width):
+            if grid[m][n] == "1" and islands[m][n] == 0:
+                
+                explore(m, n, islands, grid, counter)
+
+                for i in islands:
+                    print(i)
+
+                counter += 1
+
+    return counter - 1
+
+def explore(m, n, islands, grid, counter):
+
+    if grid[m][n] == "1" and islands[m][n] == 0:
+        islands[m][n] = counter
+
+        if m + 1 <  len(islands):
+            explore(m + 1, n, islands, grid, counter)
+
+        if n + 1 < len(islands[0]):
+            explore(m, n + 1, islands, grid, counter)
+
+        if m - 1 >= 0:
+            explore(m - 1, n, islands, grid, counter)
+
+        if n - 1 >= 0:
+            explore(m, n - 1, islands, grid, counter)
+
+
+
+
