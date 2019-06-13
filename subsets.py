26372803c205403b1759e04322005265b5836091
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-06-13 18:55:52 +0800

    subsets

---
+++ subsets.py
@@ -0,0 +1,32 @@
+# Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+# Note: The solution set must not contain duplicate subsets.
+
+# Example:
+
+# Input: nums = [1,2,3]
+# Output:
+# [
+#   [3],
+#   [1],
+#   [2],
+#   [1,2,3],
+#   [1,3],
+#   [2,3],
+#   [1,2],
+#   []
+# ]
+
+def subsets(nums):
+
+    if len(nums) < 1: return [[]]
+
+    par = nums[0]
+
+    res = self.subsets(nums[1:])
+    full = res[:]
+
+    for i in res:
+        full.append(i + [par])
+
+    return full
