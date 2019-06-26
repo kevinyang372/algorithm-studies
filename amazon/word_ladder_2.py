47b0d025c4b33f69c4b5501df96cba44f38d51a6
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-06-26 16:54:44 +0800

    word ladder 2

--- amazon/word_ladder_2.py
+++ amazon/word_ladder_2.py
@@ -67,3 +67,34 @@ def distance(w1, w2):
             d += 1
 
     return d
+
+def findLadders(beginWord, endWord, wordList):
+    
+    wordSet = set([])
+    for word in wordList:
+        wordSet.add(word)
+    
+    level = set([beginWord])
+    
+    parents = collections.defaultdict(set)
+    
+    while level and endWord not in parents:
+        next_level = collections.defaultdict(set)
+        for word in level:
+            for i in range(len(beginWord)):
+                p1 = word[:i]
+                p2 = word[i+1:]
+                for j in 'abcdefghijklmnopqrstuvwxyz':
+                    # accelerate 
+                    if word[i] != j:
+                        childWord = p1 + j + p2
+                        if childWord in wordSet and childWord not in parents:
+                            next_level[childWord].add(word)
+        level = next_level
+        parents.update(next_level)
+    
+    res = [[endWord]]
+    while res and res[0][0] !=beginWord:
+        res = [[p]+r for r in res for p in parents[r[0]]]
+    
+    return res
