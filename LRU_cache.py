cd37712caee3ba1f187c65151e28b6870da10b7d
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-07-01 14:23:50 +0800

    LRU cache OrderDict

--- LRU_cache.py
+++ LRU_cache.py
@@ -68,3 +68,26 @@ class LRUCache(object):
             self.d.pop(b)
             self.d[key] = value
             self.maxage += 1
+
+# OrderDict
+
+def __init__(self, capacity):
+    self.dic = collections.OrderedDict()
+    self.remain = capacity
+
+def get(self, key):
+    if key not in self.dic:
+        return -1
+    v = self.dic.pop(key) 
+    self.dic[key] = v   # set key as the newest one
+    return v
+
+def put(self, key, value):
+    if key in self.dic:    
+        self.dic.pop(key)
+    else:
+        if self.remain > 0:
+            self.remain -= 1  
+        else:  # self.dic is full
+            self.dic.popitem(last=False) 
+    self.dic[key] = value
