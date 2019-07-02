d9656bf63ab60a5aaad852509a77b5f454674e4e
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-07-02 10:38:37 +0800

    best_time_to_buy_and_sell_stock_2 O(N) time O(1) space

---
+++ best_time_to_buy_and_sell_stock_2.py
@@ -0,0 +1,44 @@
+# Say you have an array for which the ith element is the price of a given stock on day i.
+
+# Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+# Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+# Example 1:
+
+# Input: [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+#              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+# Example 2:
+
+# Input: [1,2,3,4,5]
+# Output: 4
+# Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+#              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+#              engaging multiple transactions at the same time. You must sell before buying again.
+# Example 3:
+
+# Input: [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+def maxProfit(prices):
+
+    if not prices: return 0
+        
+    min_value = prices[0]
+    profit = 0
+
+    for i in range(1, len(prices) - 1):
+        if prices[i] < min_value:
+            min_value = prices[i]
+        elif prices[i] > min_value:
+            if prices[i + 1] <= prices[i]:
+                profit += prices[i] - min_value
+                min_value = prices[i + 1]
+    
+    if min_value < prices[-1]:
+        profit += prices[-1] - min_value
+
+    return profit
