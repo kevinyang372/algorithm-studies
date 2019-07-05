7e2625b3f17c0d913fef049bfa8946ac7e40337c
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-07-05 10:41:20 +0800

    word break 2

---
+++ word_break_2.py
@@ -0,0 +1,59 @@
+# Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
+
+# Note:
+
+# The same word in the dictionary may be reused multiple times in the segmentation.
+# You may assume the dictionary does not contain duplicate words.
+# Example 1:
+
+# Input:
+# s = "catsanddog"
+# wordDict = ["cat", "cats", "and", "sand", "dog"]
+# Output:
+# [
+#   "cats and dog",
+#   "cat sand dog"
+# ]
+# Example 2:
+
+# Input:
+# s = "pineapplepenapple"
+# wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+# Output:
+# [
+#   "pine apple pen apple",
+#   "pineapple pen apple",
+#   "pine applepen apple"
+# ]
+# Explanation: Note that you are allowed to reuse a dictionary word.
+# Example 3:
+
+# Input:
+# s = "catsandog"
+# wordDict = ["cats", "dog", "sand", "and", "cat"]
+# Output:
+# []
+
+def wordBreak(s, wordDict):
+        res = [[] for _ in range(len(s) + 1)]
+        res[0].append('')
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if s[j:i] in wordDict and res[j]:
+                    res[i].append(s[j:i])
+
+        return joinResult(res, len(s), [])
+    
+def joinResult(res, ind, stack):
+    if ind == 0:
+        return stack
+    
+    s = []
+    for i in res[ind]:
+        if not stack:
+            s += joinResult(res, ind - len(i), [i])
+        else:
+            s += joinResult(res, ind - len(i), ["%s %s" % (i, t) for t in stack])
+    return s
+
