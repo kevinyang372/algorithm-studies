6d77affe04140e2e103e78d5080dd0da9aad38aa
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-07-15 11:17:49 +0800

    linked list random node reservoir sampling

---
+++ linked_list_random_node.py
@@ -0,0 +1,36 @@
+# Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+# Follow up:
+# What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+# Example:
+
+# // Init a singly linked list [1,2,3].
+# ListNode head = new ListNode(1);
+# head.next = new ListNode(2);
+# head.next.next = new ListNode(3);
+# Solution solution = new Solution(head);
+
+# // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+# solution.getRandom();
+
+# reservoir sampling
+
+import random
+
+class Solution(object):
+
+    def __init__(self, head):
+        self.head = head
+
+    def getRandom(self):
+        
+        res, node, count = self.head, self.head.next, 1
+
+        while node:
+            if random.randint(0, count) == 0:
+                res = node
+            node = node.next
+            count += 1
+        
+        return res.val
