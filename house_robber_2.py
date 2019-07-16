6ae6c79b3ef6709da26c1289a17f2821ad7c5383
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-07-16 17:03:21 +0800

    house robber 2

---
+++ house_robber_2.py
@@ -0,0 +1,34 @@
+# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+# Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+# Example 1:
+
+# Input: [2,3,2]
+# Output: 3
+# Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+#              because they are adjacent houses.
+# Example 2:
+
+# Input: [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+#              Total amount you can rob = 1 + 3 = 4.
+
+def rob(nums):
+
+    if not nums: return 0
+    if len(nums) < 4: return max(nums)
+
+    def helper(sub): 
+        temp = [0] * (len(sub))
+        max_res = 0
+
+        for i, v in enumerate(sub):
+            if i > 1:
+                temp[i] = max(temp[i - 1], temp[i - 2] + v)
+                max_res = max(max_res, temp[i])
+            else:
+                temp[i] = v
+
+    return helper(nums[1:], nums[:-1])
