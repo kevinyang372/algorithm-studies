9c656735ba19ca33dfc2df2d07e03697c2f66a5b
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-07-22 19:48:29 +0900

    copy list with random pointer

---
+++ copy_list_with_random_pointer.py
@@ -0,0 +1,42 @@
+# A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+# Return a deep copy of the list.
+
+ 
+
+# Example 1:
+
+
+
+# Input:
+# {"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}
+
+# Explanation:
+# Node 1's value is 1, both of its next and random pointer points to Node 2.
+# Node 2's value is 2, its next pointer points to null and its random pointer points to itself.
+ 
+
+# Note:
+
+# You must return the copy of the given head as a reference to the cloned list.
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+
+cache = {}
+def copyRandomList(head):
+
+    if not head: return
+    if head in cache: return cache[head]
+
+    h = Node(head.val, None, None)
+    cache[head] = h
+
+    h.next = copyRandomList(head.next)
+    h.random = copyRandomList(head.random)
+
+    return h
