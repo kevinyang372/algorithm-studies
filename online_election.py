5f8cb6535abd978102d0ce79803c220a09c502a7
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-08-23 17:46:26 +0800

    online election binary search

---
+++ online_election.py
@@ -0,0 +1,45 @@
+# In an election, the i-th vote was cast for persons[i] at time times[i].
+
+# Now, we would like to implement the following query function: TopVotedCandidate.q(int t) will return the number of the person that was leading the election at time t.  
+
+# Votes cast at time t will count towards our query.  In the case of a tie, the most recent vote (among tied candidates) wins.
+
+ 
+
+# Example 1:
+
+# Input: ["TopVotedCandidate","q","q","q","q","q","q"], [[[0,1,1,0,0,1,0],[0,5,10,15,20,25,30]],[3],[12],[25],[15],[24],[8]]
+# Output: [null,0,1,1,0,0,1]
+# Explanation: 
+# At time 3, the votes are [0], and 0 is leading.
+# At time 12, the votes are [0,1,1], and 1 is leading.
+# At time 25, the votes are [0,1,1,0,0,1], and 1 is leading (as ties go to the most recent vote.)
+# This continues for 3 more queries at time 15, 24, and 8.
+ 
+
+# Note:
+
+# 1 <= persons.length = times.length <= 5000
+# 0 <= persons[i] <= persons.length
+# times is a strictly increasing array with all elements in [0, 10^9].
+# TopVotedCandidate.q is called at most 10000 times per test case.
+# TopVotedCandidate.q(int t) is always called with t >= times[0].
+
+# binary search
+class TopVotedCandidate(object):
+
+    def __init__(self, persons, times):
+        self.times = times
+        self.vote = []
+        winner = None
+        d = collections.Counter()
+        
+        for i in persons:
+            d[i] += 1
+            if winner is None or d[i] >= d[winner]:
+                winner = i
+            self.vote.append(winner)
+        
+
+    def q(self, t):
+        return self.vote[bisect.bisect_right(self.times, t) - 1]
