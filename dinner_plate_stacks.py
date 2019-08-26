6f9631e3847189c3bfbf033bdf821ff2f9e9396b
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-08-26 15:54:09 +0800

    dinner plate stacks stack

--- dinner_plate_stacks.py
+++ dinner_plate_stacks.py
@@ -103,3 +103,40 @@ class DinnerPlates(object):
             return self.stack[index].pop()
         else:
             return -1
+
+# using stack
+class DinnerPlates:
+
+    def __init__(self, capacity):
+        self.queue = []
+        self.c = capacity
+        self.emp = []
+
+    def push(self, val):
+        if self.emp:
+            l = heapq.heappop(self.emp)
+            if l < len(self.queue):
+                self.queue[l] += [val]
+                return
+            else: self.emp = []
+        if len(self.queue)>0 and len(self.queue[-1]) < self.c:
+            self.queue[-1] += [val]
+            return
+        self.queue += [[val]]
+
+    def pop(self):
+        while len(self.queue) > 0 and not self.queue[-1]:
+            self.queue.pop()
+        if self.queue:
+            res = self.queue[-1][-1]
+            self.queue[-1].pop()
+            return res
+        return -1
+
+    def popAtStack(self, index):
+        if index < len(self.queue) and len(self.queue[index]) > 0:
+            res = self.queue[index][-1]
+            self.queue[index].pop()
+            heapq.heappush(self.emp,index)
+            return res
+        return -1
