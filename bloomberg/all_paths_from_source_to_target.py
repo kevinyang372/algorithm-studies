a5532eaef713f36d7a048aab29042a7dee04cff8
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-10-05 14:12:08 +0200

    all paths from source to target bfs

---
+++ bloomberg/all_paths_from_source_to_target.py
@@ -0,0 +1,40 @@
+# Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+
+# The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+
+# Example:
+# Input: [[1,2], [3], [3], []] 
+# Output: [[0,1,3],[0,2,3]] 
+# Explanation: The graph looks like this:
+# 0--->1
+# |    |
+# v    v
+# 2--->3
+# There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+# Note:
+
+# The number of nodes in the graph will be in the range [2, 15].
+# You can print different paths in any order, but you should keep the order of nodes inside one path.
+
+import collections
+
+def allPathsSourceTarget(graph):
+    if not graph: return
+
+    d = collections.deque([[0, []]])
+    res = []
+
+    while d:
+        node, path = d.popleft()
+        path.append(node)
+
+        if node == len(graph) - 1:
+            res.append(path)
+            continue
+
+
+        for i in graph[node]:
+            d.append([i, list(path)])
+
+    return res
+
