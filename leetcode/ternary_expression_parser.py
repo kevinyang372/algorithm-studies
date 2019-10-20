de7db415544c4eb66a5f4740206d2f7c43575ab2
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-10-20 14:37:24 +0200

    ternary expression parser stack

---
+++ leetcode/ternary_expression_parser.py
@@ -0,0 +1,66 @@
+# Given a string representing arbitrarily nested ternary expressions, calculate the result of the expression. You can always assume that the given expression is valid and only consists of digits 0-9, ?, :, T and F (T and F represent True and False respectively).
+
+# Note:
+
+# The length of the given string is ≤ 10000.
+# Each number will contain only one digit.
+# The conditional expressions group right-to-left (as usual in most languages).
+# The condition will always be either T or F. That is, the condition will never be a digit.
+# The result of the expression will always evaluate to either a digit 0-9, T or F.
+# Example 1:
+
+# Input: "T?2:3"
+
+# Output: "2"
+
+# Explanation: If true, then result is 2; otherwise result is 3.
+# Example 2:
+
+# Input: "F?1:T?4:5"
+
+# Output: "4"
+
+# Explanation: The conditional expressions group right-to-left. Using parenthesis, it is read/evaluated as:
+
+#              "(F ? 1 : (T ? 4 : 5))"                   "(F ? 1 : (T ? 4 : 5))"
+#           -> "(F ? 1 : 4)"                 or       -> "(T ? 4 : 5)"
+#           -> "4"                                    -> "4"
+# Example 3:
+
+# Input: "T?T?F:5:3"
+
+# Output: "F"
+
+# Explanation: The conditional expressions group right-to-left. Using parenthesis, it is read/evaluated as:
+
+#              "(T ? (T ? F : 5) : 3)"                   "(T ? (T ? F : 5) : 3)"
+#           -> "(T ? F : 3)"                 or       -> "(T ? F : 5)"
+#           -> "F"                                    -> "F"
+
+# stack approach
+def parseTernary(self, expression):
+        
+    expression = expression[::-1]
+    stack = []
+    i = 0
+    
+    while i < len(expression):
+        if expression[i] == ':':
+            i += 1
+            continue
+        elif expression[i] == '?':
+            option1 = stack.pop()
+            option2 = stack.pop()
+            
+            if expression[i + 1] == 'T':
+                stack.append(option1)
+            else:
+                stack.append(option2)
+                
+            i = i + 1
+        else:
+            stack.append(expression[i])
+            
+        i += 1
+        
+    return stack[-1]
