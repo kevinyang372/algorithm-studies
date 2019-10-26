a6c889537c66bc5b6efc642c81ebf83b229ad6c1
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-10-26 11:50:56 +0200

    concatenate words

---
+++ facebook/concatenate_words.py
@@ -0,0 +1,28 @@
+# Given a list of words (without duplicates), please write a program that returns all concatenated words in the given list of words.
+# A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+
+# Example:
+# Input: ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+
+# Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+
+# Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
+#  "dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
+# "ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+# Note:
+# The number of elements of the given array will not exceed 10,000
+# The length sum of elements in the given array will not exceed 600,000.
+# All the input string will only include lower case letters.
+# The returned elements order does not matter.
+
+def findAllConcatenatedWordsInADict(self, words):
+        
+    words = set(words)
+    
+    def search(w):
+        for i in range(1, len(w)):
+            if w[:i] in words and (w[i:] in words or search(w[i:])):
+                return True
+        return False
+    
+    return [word for word in words if search(word)]
