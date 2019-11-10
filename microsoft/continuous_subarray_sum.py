9e2a72fc2c884d974337a054689df424611bab3a
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-11-11 00:43:12 +0100

    continuous subarray sum

---
+++ microsoft/continuous_subarray_sum.py
@@ -0,0 +1,38 @@
+# Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of size at least 2 that sums up to a multiple of k, that is, sums up to n*k where n is also an integer.
+
+ 
+
+# Example 1:
+
+# Input: [23, 2, 4, 6, 7],  k=6
+# Output: True
+# Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+# Example 2:
+
+# Input: [23, 2, 6, 4, 7],  k=6
+# Output: True
+# Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+ 
+
+# Note:
+
+# The length of the array won't exceed 10,000.
+# You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
+
+def checkSubarraySum(self, nums, k):
+        
+    if len(nums) < 2: return False
+    
+    sums = [0] * (len(nums) + 1)
+    sums[0] = 0
+    
+    for i in range(1, len(nums) + 1):
+        sums[i] = sums[i - 1] + nums[i - 1]
+        for j in range(i - 1):
+            if k == 0:
+                if sums[i] - sums[j] == 0:
+                    return True
+            elif (sums[i] - sums[j]) % k == 0:
+                return True
+            
+    return False
