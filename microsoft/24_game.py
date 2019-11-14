a8f06c5142e13e2f4f0bd99b983312f8f949f4e9
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-11-14 20:33:51 +0100

    24 game

---
+++ microsoft/24_game.py
@@ -0,0 +1,35 @@
+# You have 4 cards each containing a number from 1 to 9. You need to judge whether they could operated through *, /, +, -, (, ) to get the value of 24.
+
+# Example 1:
+# Input: [4, 1, 8, 7]
+# Output: True
+# Explanation: (8-4) * (7-1) = 24
+# Example 2:
+# Input: [1, 2, 1, 2]
+# Output: False
+# Note:
+# The division operator / represents real division, not integer division. For example, 4 / (1 - 2/3) = 12.
+# Every operation done is between two numbers. In particular, we cannot use - as a unary operator. For example, with [1, 1, 1, 1] as input, the expression -1 - 1 - 1 - 1 is not allowed.
+# You cannot concatenate numbers together. For example, if the input is [1, 2, 1, 2], we cannot write this as 12 + 12.
+
+def judgePoint24(self, nums: List[int]) -> bool:
+    def dfs(arr):
+        if len(arr) == 1:
+            return math.isclose(arr[0], 24.0)
+        
+        nex = [(i, t) for i in range(len(arr) - 1) for t in range(i + 1, len(arr))]
+        for x, y in nex:
+            rest = [i for i in range(len(arr)) if i != x and i != y]
+            for operator in ["*", "/", "+", "-"]:
+                if operator != '/' or arr[y] != 0:
+                    new_arr = [eval("%s%sfloat(%s)" % (arr[x], operator, float(arr[y])))] + [arr[t] for t in rest]                
+                    if dfs(new_arr): return True
+                
+                if operator in ["/", "-"]:
+                    if operator != '/' or arr[x] != 0:
+                        new_arr = [eval("%s%sfloat(%s)" % (arr[y], operator, arr[x]))] + [arr[t] for t in rest]                
+                        if dfs(new_arr): return True
+                    
+        return False
+    
+    return dfs(nums)
