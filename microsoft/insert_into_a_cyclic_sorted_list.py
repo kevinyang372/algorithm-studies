c76acde37bc59c83c65c2ecdc4c3d1290a9a3b94
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-11-14 14:55:12 +0100

    insert into a cyclic sorted list

---
+++ microsoft/insert_into_a_cyclic_sorted_list.py
@@ -0,0 +1,52 @@
+# Given a node from a cyclic linked list which is sorted in ascending order, write a function to insert a value into the list such that it remains a cyclic sorted list. The given node can be a reference to any single node in the list, and may not be necessarily the smallest value in the cyclic list.
+
+# If there are multiple suitable places for insertion, you may choose any place to insert the new value. After the insertion, the cyclic list should remain sorted.
+
+# If the list is empty (i.e., given node is null), you should create a new single cyclic list and return the reference to that single node. Otherwise, you should return the original given node.
+
+# The following example may help you understand the problem better:
+
+ 
+
+
+
+# In the figure above, there is a cyclic sorted list of three elements. You are given a reference to the node with value 3, and we need to insert 2 into the list.
+
+ 
+
+
+
+# The new node should insert between node 1 and node 3. After the insertion, the list should look like this, and we should still return node 3.
+
+def insert(self, head, insertVal):
+        
+    node = Node(insertVal)
+    
+    if not head:
+        node.next = node
+        return node
+    elif head.next == head:
+        head.next = node
+        node.next = head
+        return node
+    
+    root = head
+    prev = head
+    head = head.next
+    
+    max_node = head
+    
+    while (prev.val > insertVal or head.val < insertVal) and head != root:
+        if head.val >= max_node.val:
+            max_node = head
+        prev, head = head, head.next
+    
+    if head.val >= max_node.val:
+        max_node = head
+    
+    if head != root or (prev.val <= insertVal and head.val >= insertVal):
+        prev.next, node.next = node, head
+    else:
+        max_node.next, node.next = node, max_node.next
+        
+    return root
