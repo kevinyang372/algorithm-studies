6939a10eec7aeb695da6202ef1fe40d332fb4e67
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-11-14 12:39:13 +0100

    implement magic dictionary

---
+++ microsoft/implement_magic_dictionary.py
@@ -0,0 +1,62 @@
+# Implement a magic directory with buildDict, and search methods.
+
+# For the method buildDict, you'll be given a list of non-repetitive words to build a dictionary.
+
+# For the method search, you'll be given a word, and judge whether if you modify exactly one character into another character in this word, the modified word is in the dictionary you just built.
+
+# Example 1:
+# Input: buildDict(["hello", "leetcode"]), Output: Null
+# Input: search("hello"), Output: False
+# Input: search("hhllo"), Output: True
+# Input: search("hell"), Output: False
+# Input: search("leetcoded"), Output: False
+# Note:
+# You may assume that all the inputs are consist of lowercase letters a-z.
+# For contest purpose, the test data is rather small by now. You could think about highly efficient algorithm after the contest.
+# Please remember to RESET your class variables declared in class MagicDictionary, as static/class variables are persisted across multiple test cases. Please see here for more details.
+
+class TrieNode:
+    def __init__(self, val):
+        self.val = val
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode("")
+        
+    def insert(self, word):
+        head = self.root
+        
+        for i in word:
+            if i not in head.children:
+                head.children[i] = TrieNode(i)
+            head = head.children[i]
+        
+        head.end = True
+                
+
+class MagicDictionary(object):
+
+    def __init__(self):
+        self.trie = Trie()
+        
+
+    def buildDict(self, dict):
+        for i in dict:
+            self.trie.insert(i)
+        
+
+    def search(self, word):
+        
+        def dfs(node, w, has_one):
+            if not w: return has_one and node.end
+            if w[0] in node.children and dfs(node.children[w[0]], w[1:], has_one):
+                return True
+            if not has_one:
+                for n in node.children:
+                    if n != w[0] and dfs(node.children[n], w[1:], True):
+                        return True
+            return False
+        
+        return dfs(self.trie.root, word, False)
