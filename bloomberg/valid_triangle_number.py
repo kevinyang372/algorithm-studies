f51999880d0edbf34416db7abca0ccc226bdd565
kevinyang372 <yunfan.yang@minerva.kgi.edu>
2019-11-25 10:46:32 +0100

    valid triangle number

--- bloomberg/valid_triangle_number.py
+++ bloomberg/valid_triangle_number.py
@@ -23,3 +23,42 @@ def triangleNumber(self, nums):
             res += max(upper - lower, 0)
                     
     return res
+
+# TLE two pointers
+def triangleNumber(self, nums: List[int]) -> int:
+        
+    if len(nums) < 3: return 0
+    
+    nums.sort()
+    c = 0
+    
+    for t in range(len(nums) - 1, 1, -1):
+        i, j = 0, 1
+        while j < t:
+            if nums[i] + nums[j] <= nums[t]:
+                j += 1
+                if j == t:
+                    i += 1
+                    j = i + 1
+            else:
+                c += t - j
+                i += 1
+                j = i + 1
+                
+    return c
+
+# binary search
+def triangleNumber(self, nums: List[int]) -> int:
+    
+    if len(nums) < 3: return 0
+    
+    nums.sort()
+    c = 0
+    
+    for t in range(len(nums) - 1, 1, -1):
+        for i in range(t - 1):
+            j = i + bisect.bisect_right(nums[i + 1:t - 1], nums[t] - nums[i]) + 1
+            if j < t and nums[j] + nums[i] > nums[t]:
+                c += t - j
+                
+    return c
