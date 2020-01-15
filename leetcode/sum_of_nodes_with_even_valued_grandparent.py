b865e2e616ebeaf3a4b27891638bcf22a7bf9718
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-01-15 10:42:56 -0300

    sum of nodes with even valued grandparent

---
+++ leetcode/sum_of_nodes_with_even_valued_grandparent.py
@@ -0,0 +1,30 @@
+# Given a binary tree, return the sum of values of nodes with even-valued grandparent.  (A grandparent of a node is the parent of its parent, if it exists.)
+
+# If there are no nodes with an even-valued grandparent, return 0.
+
+ 
+
+# Example 1:
+
+
+
+# Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+# Output: 18
+# Explanation: The red nodes are the nodes with even-value grandparent while the blue nodes are the even-value grandparents.
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is between 1 and 10^4.
+# The value of nodes is between 1 and 100.
+
+def sumEvenGrandparent(self, root):
+        
+    def traverse(node, parent, grandparent):
+        if not node: return 0
+        if grandparent and grandparent % 2 == 0:
+            return node.val + traverse(node.left, node.val, parent) + traverse(node.right, node.val, parent)
+        else:
+            return traverse(node.left, node.val, parent) + traverse(node.right, node.val, parent)
+        
+    return traverse(root, None, None)
