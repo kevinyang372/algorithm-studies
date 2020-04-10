ef7310a59952334bd15740a6a95fdaee63cd6e8a
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-04-10 17:34:15 -0300

    stone game 3

---
+++ leetcode/stone_game_3.py
@@ -0,0 +1,77 @@
+# Alice and Bob continue their games with piles of stones. There are several stones arranged in a row, and each stone has an associated value which is an integer given in the array stoneValue.
+
+# Alice and Bob take turns, with Alice starting first. On each player's turn, that player can take 1, 2 or 3 stones from the first remaining stones in the row.
+
+# The score of each player is the sum of values of the stones taken. The score of each player is 0 initially.
+
+# The objective of the game is to end with the highest score, and the winner is the player with the highest score and there could be a tie. The game continues until all the stones have been taken.
+
+# Assume Alice and Bob play optimally.
+
+# Return "Alice" if Alice will win, "Bob" if Bob will win or "Tie" if they end the game with the same score.
+
+ 
+
+# Example 1:
+
+# Input: values = [1,2,3,7]
+# Output: "Bob"
+# Explanation: Alice will always lose. Her best move will be to take three piles and the score become 6. Now the score of Bob is 7 and Bob wins.
+# Example 2:
+
+# Input: values = [1,2,3,-9]
+# Output: "Alice"
+# Explanation: Alice must choose all the three piles at the first move to win and leave Bob with negative score.
+# If Alice chooses one pile her score will be 1 and the next move Bob's score becomes 5. The next move Alice will take the pile with value = -9 and lose.
+# If Alice chooses two piles her score will be 3 and the next move Bob's score becomes 3. The next move Alice will take the pile with value = -9 and also lose.
+# Remember that both play optimally so here Alice will choose the scenario that makes her win.
+# Example 3:
+
+# Input: values = [1,2,3,6]
+# Output: "Tie"
+# Explanation: Alice cannot win this game. She can end the game in a draw if she decided to choose all the first three piles, otherwise she will lose.
+# Example 4:
+
+# Input: values = [1,2,3,-1,-2,-3,7]
+# Output: "Alice"
+# Example 5:
+
+# Input: values = [-1,-2,-3]
+# Output: "Tie"
+ 
+
+# Constraints:
+
+# 1 <= values.length <= 50000
+# -1000 <= values[i] <= 1000
+
+def stoneGameIII(self, stoneValue: List[int]) -> str:
+        
+    threshold = sum(stoneValue)
+    d = {}
+    
+    def traverse(i):
+        if i in d: return d[i]
+        if i >= len(stoneValue): return 0
+        
+        max_val = -float('inf')
+        curr = 0
+        
+        for t in range(i, i + 3):
+            if t >= len(stoneValue): break
+            curr += stoneValue[t]
+            min_val = float('inf')
+            for j in range(t + 2, t + 5):
+                min_val = min(min_val, traverse(j))
+            max_val = max(max_val, curr + min_val)
+        
+        d[i] = max_val
+        return max_val
+    
+    res = traverse(0)
+    if res > threshold / 2:
+        return "Alice"
+    elif res == threshold / 2:
+        return "Tie"
+    else:
+        return "Bob"
