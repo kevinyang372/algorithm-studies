cc3b447e3422e2665417bc039227f21f9d047d3c
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-07-07 19:35:35 -0700

    count submatrices with all ones

--- leetcode/count_submatrices_with_all_ones.py
+++ leetcode/count_submatrices_with_all_ones.py
@@ -46,6 +46,7 @@
 # 1 <= columns <= 150
 # 0 <= mat[i][j] <= 1
 
+# O(N^4)
 def numSubmat(self, mat: List[List[int]]) -> int:
         
     d = set()
@@ -62,3 +63,23 @@ def numSubmat(self, mat: List[List[int]]) -> int:
             check_size(h, w)
     
     return len(d)
+
+
+# O(N^3)
+def numSubmat(self, mat: List[List[int]]) -> int:
+        
+    m, n = len(mat), len(mat[0])
+    count = 0
+    for left in range(n):
+        row_sums = [0 for _ in range(m)]
+        for right in range(left, n):
+            consec_count = 0
+            width = right - left + 1
+            for bottom in range(m):
+                row_sums[bottom] += mat[bottom][right]
+                if row_sums[bottom] != width:
+                    consec_count = 0
+                else:
+                    consec_count += 1
+                    count += consec_count
+    return count
