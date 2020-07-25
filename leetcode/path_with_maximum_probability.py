1afbc1e51f652a931f3edfded90256190d6b24c4
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-07-25 14:40:22 -0700

    path with maximum probability

---
+++ leetcode/path_with_maximum_probability.py
@@ -0,0 +1,62 @@
+# You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
+
+# Given two nodes start and end, find the path with the maximum probability of success to go from start to end and return its success probability.
+
+# If there is no path from start to end, return 0. Your answer will be accepted if it differs from the correct answer by at most 1e-5.
+
+ 
+
+# Example 1:
+
+
+
+# Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.2], start = 0, end = 2
+# Output: 0.25000
+# Explanation: There are two paths from start to end, one having a probability of success = 0.2 and the other has 0.5 * 0.5 = 0.25.
+# Example 2:
+
+
+
+# Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.3], start = 0, end = 2
+# Output: 0.30000
+# Example 3:
+
+
+
+# Input: n = 3, edges = [[0,1]], succProb = [0.5], start = 0, end = 2
+# Output: 0.00000
+# Explanation: There is no path between 0 and 2.
+ 
+
+# Constraints:
+
+# 2 <= n <= 10^4
+# 0 <= start, end < n
+# start != end
+# 0 <= a, b < n
+# a != b
+# 0 <= succProb.length == edges.length <= 2*10^4
+# 0 <= succProb[i] <= 1
+# There is at most one edge between every two nodes.
+
+def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        
+    d = collections.defaultdict(list)
+    for i in range(len(edges)):
+        x, y = edges[i]
+        d[x].append((y, succProb[i]))
+        d[y].append((x, succProb[i]))
+        
+    visited = set()
+    queue = [(-1.0, start)]
+    
+    while queue:
+        prob, curr = heapq.heappop(queue)
+        if curr == end: return -prob
+        visited.add(curr)
+
+        for next, p in d[curr]:
+            if next not in visited: 
+                heapq.heappush(queue, (p * prob, next))
+                    
+    return 0.0
