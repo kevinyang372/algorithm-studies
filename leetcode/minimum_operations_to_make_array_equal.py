e051113710f91fb08c2d4596c1644d081e1977f5
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-08-16 17:33:04 -0700

    minimum operations to make array equal

---
+++ leetcode/minimum_operations_to_make_array_equal.py
@@ -0,0 +1,45 @@
+# You have an array arr of length n where arr[i] = (2 * i) + 1 for all valid values of i (i.e. 0 <= i < n).
+
+# In one operation, you can select two indices x and y where 0 <= x, y < n and subtract 1 from arr[x] and add 1 to arr[y] (i.e. perform arr[x] -=1 and arr[y] += 1). The goal is to make all the elements of the array equal. It is guaranteed that all the elements of the array can be made equal using some operations.
+
+# Given an integer n, the length of the array. Return the minimum number of operations needed to make all the elements of arr equal.
+
+ 
+
+# Example 1:
+
+# Input: n = 3
+# Output: 2
+# Explanation: arr = [1, 3, 5]
+# First operation choose x = 2 and y = 0, this leads arr to be [2, 3, 4]
+# In the second operation choose x = 2 and y = 0 again, thus arr = [3, 3, 3].
+# Example 2:
+
+# Input: n = 6
+# Output: 9
+ 
+
+# Constraints:
+
+# 1 <= n <= 10^4
+
+def minOperations(self, n: int) -> int:
+        
+    get_number = lambda x: 2 * x + 1
+    
+    if n % 2 == 0:
+        i, j = (n - 1) // 2, n // 2
+        mid = (get_number(i) + get_number(j)) // 2
+        count = 1
+    else:
+        i = j = n // 2
+        mid = get_number(i)
+        count = 0
+    
+    i -= 1
+    
+    while i >= 0:
+        count += mid - get_number(i)
+        i -= 1
+    
+    return count
