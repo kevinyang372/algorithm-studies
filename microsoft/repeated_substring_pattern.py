b73379fc801a9b814812c1f0bccd311323eb3d9d
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-09-03 23:38:06 -0700

    repeated substring pattern

--- microsoft/repeated_substring_pattern.py
+++ microsoft/repeated_substring_pattern.py
@@ -24,3 +24,27 @@ def repeatedSubstringPattern(self, s: str) -> bool:
             return True
         i += 1
     return False
+
+# eliminate pattern one by one
+def repeatedSubstringPattern(self, s: str) -> bool:
+    curr = []
+    pattern = ''
+    d = set()
+    
+    for char in s:
+        pattern += char
+        
+        for ind in range(len(curr)):
+            if curr[ind][2]:
+                curr[ind][0] += char
+                if len(curr[ind][0]) == curr[ind][1]:
+                    if curr[ind][0] in d:
+                        curr[ind][0] = ''
+                    else:
+                        curr[ind][2] = False
+        
+        if len(pattern) != len(s) and len(s) % len(pattern) == 0:
+            d.add(pattern)
+            curr.append(['', len(pattern), True])
+
+    return any(pat[2] for pat in curr)
