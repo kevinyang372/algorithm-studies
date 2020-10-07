ea18c63fb30555a1d2d3f1c438ccb6ce5ece0ec4
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-10-06 17:29:43 -0700

    producer consumer with condition variable

--- SystemDesign/producer_consumer.py
+++ SystemDesign/producer_consumer.py
@@ -34,8 +34,42 @@ class ProducerConsumer:
             time.sleep(1)
 
 
+class ProducerConsumerMonitor:
+
+    def __init__(self):
+        self.to_consume = 0
+        self.producer_lock = threading.Condition()
+
+    def produce(self):
+        for i in range(1, 6):
+            self.producer_lock.acquire()
+
+            self.to_consume = i
+            print(f"Produced {self.to_consume}")
+            self.producer_lock.notify_all()
+            self.producer_lock.release()
+
+            time.sleep(2)
+
+
+    def consume(self):
+        for i in range(5):
+            self.producer_lock.acquire()
+            
+            consumed = self.to_consume
+            print(f"Consumed {consumed}")
+            self.producer_lock.release()
+            
+            time.sleep(1)
+
+            self.producer_lock.acquire()
+            while self.to_consume == consumed and i != 4:
+                self.producer_lock.wait()
+            self.producer_lock.release()
+
+
 if __name__ == "__main__":
-    pc = ProducerConsumer()
+    pc = ProducerConsumerMonitor()
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         executor.submit(pc.produce)
