e99b7a4720383c0a275df9fa9c99df03171c894d
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-10-13 14:10:49 -0700

    uber ride problem with semaphore

--- SystemDesign/uber_ride_problem.py
+++ SystemDesign/uber_ride_problem.py
@@ -1,7 +1,7 @@
 # Imagine at the end of a political conference, republicans and democrats are trying to leave the venue and ordering Uber rides at the same time. However, to make sure no fight breaks out in an Uber ride, the software developers at Uber come up with an algorithm whereby either an Uber ride can have all democrats or republicans or two Democrats and two Republicans. All other combinations can result in a fist-fight.
 
 # Your task as the Uber developer is to model the ride requestors as threads. Once an acceptable combination of riders is possible, threads are allowed to proceed to ride. Each thread invokes the method seated() when selected by the system for the next ride. When all the threads are seated, any one of the four threads can invoke the method drive() to inform the driver to start the ride.
-from threading import Thread, Condition, Lock, current_thread
+from threading import Thread, Condition, Lock, Semaphore, Barrier, current_thread
 import random
 
 class UberSeatingProblem:
@@ -69,9 +69,98 @@ class UberSeatingProblem:
             self.seat(ride)
 
 
+class UberSeatingProblem_Semaphore:
+
+    def __init__(self):
+        self.democrat_count = self.republican_count = 0
+        self.democrat_lock = Semaphore(0)
+        self.republican_lock = Semaphore(0)
+
+        self.lock = Lock()
+        self.barrier = Barrier(4)
+
+        self.ride_id = 0
+
+    def drive(self):
+        print(f"Ride {self.ride_id} is on its way!")
+        self.ride_id += 1
+
+    def seat(self, party):
+        print(f"Seated one {party} in {self.ride_id}")
+
+    def seat_democrat(self):
+        ride_leader = False
+        self.lock.acquire()
+        self.democrat_count += 1
+
+        if self.democrat_count == 4:
+            self.democrat_count = 0
+
+            self.democrat_lock.release()
+            self.democrat_lock.release()
+            self.democrat_lock.release()
+
+            ride_leader = True
+
+        elif self.democrat_count == 2 and self.republican_count >= 2:
+            self.democrat_count -= 2
+            self.republican_count -= 2
+
+            self.democrat_lock.release()
+            self.republican_lock.release()
+            self.republican_lock.release()
+
+            ride_leader = True
+
+        else:
+            self.lock.release()
+            self.democrat_lock.acquire()
+
+        self.seat("Democrat")
+        self.barrier.wait()
+
+        if ride_leader:
+            self.drive()
+            self.lock.release()
+
+    def seat_republican(self):
+        ride_leader = False
+        self.lock.acquire()
+        self.republican_count += 1
+
+        if self.republican_count == 4:
+            self.republican_count = 0
+
+            self.republican_lock.release()
+            self.republican_lock.release()
+            self.republican_lock.release()
+
+            ride_leader = True
+
+        elif self.republican_count == 2 and self.democrat_count >= 2:
+            self.democrat_count -= 2
+            self.republican_count -= 2
+
+            self.republican_lock.release()
+            self.democrat_lock.release()
+            self.democrat_lock.release()
+
+            ride_leader = True
+
+        else:
+            self.lock.release()
+            self.republican_lock.acquire()
+
+        self.seat("Republican")
+        self.barrier.wait()
+
+        if ride_leader:
+            self.drive()
+            self.lock.release()
+
 
 def controlled_simulation():
-    problem = UberSeatingProblem()
+    problem = UberSeatingProblem_Semaphore()
     dems = 10
     repubs = 10
     
