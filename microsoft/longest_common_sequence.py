04ba41b722f1df35d601adbc2bc7019e2f1a0d4e
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-10-26 00:33:41 -0700

    longest common sequence

--- microsoft/longest_common_sequence.py
+++ microsoft/longest_common_sequence.py
@@ -45,3 +45,17 @@ def longestCommonSubsequence(self, text1, text2):
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
                 
     return dp[-1][-1]
+
+# shorter
+def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+    dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+    
+    for i in range(len(text1)):
+        for j in range(len(text2)):
+            if text1[i] == text2[j]:
+                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j], dp[i][j] + 1)
+            else:
+                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j], dp[i][j])
+    
+    return dp[-1][-1]
