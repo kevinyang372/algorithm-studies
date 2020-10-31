edf12dcf92390a02802654abc288cc42cfa8abf4
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-10-30 18:20:11 -0700

    summary_ranges

--- leetcode/summary_ranges.py
+++ leetcode/summary_ranges.py
@@ -29,3 +29,22 @@ def summaryRanges(self, nums):
             res.append("%s->%s" % (x, y))
             
     return res
+
+def summaryRanges(self, nums: List[int]) -> List[str]:
+    res = []
+    
+    i = 0
+    while i < len(nums):
+        j = i + 1
+        
+        while j < len(nums) and nums[j] == nums[j - 1] + 1:
+            j += 1
+        
+        if i != j - 1:
+            res.append(f"{nums[i]}->{nums[j - 1]}")
+        else:
+            res.append(f"{nums[i]}")
+        
+        i = j
+    
+    return res
