c40c29f9ee8f46845f84c544448c3ce4d63377d9
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-11-09 16:11:51 -0800

    maximum difference between node and ancester

---
+++ leetcode/maximum_difference_between_node_and_ancester.py
@@ -0,0 +1,53 @@
+# Given the root of a binary tree, find the maximum value V for which there exist different nodes A and B where V = |A.val - B.val| and A is an ancestor of B.
+
+# A node A is an ancestor of B if either: any child of A is equal to B, or any child of A is an ancestor of B.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
+# Output: 7
+# Explanation: We have various ancestor-node differences, some of which are given below :
+# |8 - 3| = 5
+# |3 - 7| = 4
+# |8 - 1| = 7
+# |10 - 13| = 3
+# Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+# Example 2:
+
+
+# Input: root = [1,null,2,null,0,3]
+# Output: 3
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [2, 5000].
+# 0 <= Node.val <= 105
+
+def maxAncestorDiff(self, root: TreeNode) -> int:
+        
+    d = {'max_val': 0}
+    
+    def search(node):
+        min_val = max_val = node.val
+        
+        temp_left_min = temp_right_min = float('inf')
+        temp_left_max = temp_right_max = -float('inf')
+        
+        if node.left:
+            temp_left_min, temp_left_max = search(node.left)
+        
+        if node.right:
+            temp_right_min, temp_right_max = search(node.right)
+            
+        min_val = min(min_val, temp_left_min, temp_right_min)
+        max_val = max(max_val, temp_left_max, temp_right_max)
+        
+        d['max_val'] = max(d['max_val'], abs(max_val - node.val), abs(node.val - min_val))
+        return min_val, max_val
+    
+    search(root)
+    return d['max_val']
