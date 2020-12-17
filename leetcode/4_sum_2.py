6509712f96d239fb2fc12f7b66a20a3035f81dd5
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-12-17 12:54:22 -0800

    4sum II

--- leetcode/4_sum_2.py
+++ leetcode/4_sum_2.py
@@ -18,19 +18,40 @@
 # 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
 # 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
 
+
 def fourSumCount(self, A, B, C, D):
-    
+
     d = collections.Counter()
-    
+
     for i in A:
         for t in B:
             d[i + t] += 1
-    
+
     res = 0
-    
+
     for m in C:
         for n in D:
             if -m - n in d:
                 res += d[-m - n]
-    
+
     return res
+
+
+def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+
+    def get_two_sum_counter(l1, l2):
+        c = collections.Counter()
+        c[0] = 1
+
+        for lis in [l1, l2]:
+            new_c = collections.Counter()
+            for v1 in c:
+                for v2 in lis:
+                    new_c[v1 + v2] += c[v1]
+            c = new_c
+
+        return c
+
+    d1 = get_two_sum_counter(A, B)
+    d2 = get_two_sum_counter(C, D)
+    return sum(d1[val] * d2[-val] for val in d1)
