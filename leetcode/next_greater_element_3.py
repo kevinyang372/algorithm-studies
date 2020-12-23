c5e84af5143be523582452b7d2c0cb2fe0a62a5f
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-12-23 14:58:11 -0800

    next greater element 3

--- leetcode/next_greater_element_3.py
+++ leetcode/next_greater_element_3.py
@@ -4,7 +4,7 @@
 
 # Input: 12
 # Output: 21
- 
+
 
 # Example 2:
 
@@ -12,12 +12,13 @@
 # Output: -1
 
 def nextGreaterElement(self, n):
-        
-    if n <= 10: return -1
-    
+
+    if n <= 10:
+        return -1
+
     n, stack = n // 10, [n % 10]
     found = False
-    
+
     while n:
         cur, n = n % 10, n // 10
         if cur < stack[-1]:
@@ -25,17 +26,37 @@ def nextGreaterElement(self, n):
             break
         else:
             stack.append(cur)
-            
+
     if not found:
         return -1
-            
+
     ind = bisect.bisect(stack, cur)
     n = n * 10 + stack[ind]
     stack[ind] = cur
     i = 0
-    
+
     for i in stack:
         n = n * 10 + i
 
-    if n > 2 ** 31 - 1: return -1
+    if n > 2 ** 31 - 1:
+        return -1
     return n
+
+
+def nextGreaterElement(self, n: int) -> int:
+    n = list(str(n))
+
+    stack = []
+    for i in range(len(n) - 1, 0, -1):
+        if n[i] > n[i - 1]:
+
+            t = i
+            while t < len(n) and n[i - 1] < n[t]:
+                t += 1
+
+            n[t - 1], n[i - 1] = n[i - 1], n[t - 1]
+            n[i:] = sorted(n[i:])
+
+            num = int("".join(n))
+            return num if num <= 2 ** 31 - 1 else -1 
+    return -1
