3db8948c1ee3db71f10e3a7728430a1168db7310
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2021-01-01 18:28:23 -0800

    check array formation through concatenation

---
+++ leetcode/check_array_formation_through_concatenation.py
@@ -0,0 +1,67 @@
+# You are given an array of distinct integers arr and an array of integer arrays pieces, where the integers in pieces are distinct. Your goal is to form arr by concatenating the arrays in pieces in any order. However, you are not allowed to reorder the integers in each array pieces[i].
+
+# Return true if it is possible to form the array arr from pieces. Otherwise, return false.
+
+
+# Example 1:
+
+# Input: arr = [85], pieces = [[85]]
+# Output: true
+# Example 2:
+
+# Input: arr = [15,88], pieces = [[88],[15]]
+# Output: true
+# Explanation: Concatenate [15] then [88]
+# Example 3:
+
+# Input: arr = [49,18,16], pieces = [[16,18,49]]
+# Output: false
+# Explanation: Even though the numbers match, we cannot reorder pieces[0].
+# Example 4:
+
+# Input: arr = [91,4,64,78], pieces = [[78],[4,64],[91]]
+# Output: true
+# Explanation: Concatenate [91] then [4,64] then [78]
+# Example 5:
+
+# Input: arr = [1,3,5,7], pieces = [[2,4,6,8]]
+# Output: false
+
+
+# Constraints:
+
+# 1 <= pieces.length <= arr.length <= 100
+# sum(pieces[i].length) == arr.length
+# 1 <= pieces[i].length <= arr.length
+# 1 <= arr[i], pieces[i][j] <= 100
+# The integers in arr are distinct.
+# The integers in pieces are distinct (i.e., If we flatten pieces in a 1D array, all the integers in this array are distinct).
+
+def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+    d = collections.defaultdict(list)
+
+    for i, v in enumerate(pieces):
+        d[v[0]].append(i)
+
+    def traverse(i, visited):
+        if i == len(arr):
+            return True
+
+        possible_next = d[arr[i]]
+        for next in possible_next:
+            if next not in visited:
+                di = i
+                for ele in pieces[next]:
+                    if arr[di] == ele:
+                        di += 1
+                    else:
+                        break
+                else:
+                    visited.add(next)
+                    if traverse(di, visited):
+                        return True
+                    visited.remove(next)
+
+        return False
+
+    return traverse(0, set())
