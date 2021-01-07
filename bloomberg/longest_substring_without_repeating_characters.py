ca168e0ce91e8bda14ba93d9a861911c0a1c5c82
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2021-01-07 15:59:26 -0800

    longest substring without repeating characters

--- bloomberg/longest_substring_without_repeating_characters.py
+++ bloomberg/longest_substring_without_repeating_characters.py
@@ -17,15 +17,17 @@
 # Explanation: The answer is "wke", with the length of 3. 
 #              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+
 def lengthOfLongestSubstring(self, s):
-    if not s: return 0
-    
+    if not s:
+        return 0
+
     i, j = 0, 1
     d = {}
-    
+
     d[s[i]] = i
     max_l = 1
-    
+
     while j < len(s):
         if s[j] not in d:
             d[s[j]] = j
@@ -34,8 +36,26 @@ def lengthOfLongestSubstring(self, s):
         else:
             for t in range(i, d[s[j]]):
                 d.pop(s[t], None)
-            
+
             i, d[s[j]] = d[s[j]] + 1, j
             j += 1
-            
+
     return max_l
+
+
+def lengthOfLongestSubstring(self, s: str) -> int:
+    d = set()
+    i = 0
+    max_len = 0
+
+    for j in range(len(s)):
+        if s[j] not in d:
+            d.add(s[j])
+        else:
+            while i <= j and s[i] != s[j]:
+                d.remove(s[i])
+                i += 1
+            i += 1
+        max_len = max(max_len, j - i + 1)
+
+    return max_len
