7dbf1f68b79920bb2e54e881e4ecfc7c86deae11
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2021-01-13 19:14:15 -0800

    alternative create new node

--- rust/merge_two_lists.rs
+++ rust/merge_two_lists.rs
@@ -35,3 +35,39 @@ impl Solution {
         dummy_head.next
     }
 }
+
+
+// Alternative - create new node
+impl Solution {
+    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut dummy_head = ListNode::new(0);
+        let mut current = &mut dummy_head;
+        
+        let mut p = l1;
+        let mut q = l2;
+        
+        while p != None && q != None {
+            
+            let mut p_val = p.as_mut().unwrap().val;
+            let mut q_val = q.as_mut().unwrap().val;
+            
+            if p_val < q_val {
+                current.next = Some(Box::new(ListNode::new(p_val)));
+                p = p.unwrap().next;
+            } else {
+                current.next = Some(Box::new(ListNode::new(q_val)));
+                q = q.unwrap().next;
+            }
+            
+            current = current.next.as_mut().unwrap();
+        }
+        
+        if p != None {
+            current.next = p;
+        } else {
+            current.next = q;
+        }
+        
+        dummy_head.next
+    }
+}
