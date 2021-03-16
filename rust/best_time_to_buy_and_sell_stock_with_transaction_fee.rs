2a91fcac01dfe2543ffa22488c0801c74e8fbc1b
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2021-03-16 13:06:59 -0700

    improve upon prev

--- rust/best_time_to_buy_and_sell_stock_with_transaction_fee.rs
+++ rust/best_time_to_buy_and_sell_stock_with_transaction_fee.rs
@@ -1,13 +1,11 @@
-use std::i32;
-
 impl Solution {
     pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
         let mut cash = 0i32;
-        let mut hold = -5 * 10i32.pow(4);
+        let mut hold: Option<i32> = None;
         
         for price in prices {
-            cash = cash.max(hold + price - fee);
-            hold = hold.max(cash - price);
+            cash = cash.max(hold.unwrap_or(-price) + price - fee);
+            hold = Some(hold.unwrap_or(-price).max(cash - price));
         }
         
         cash
